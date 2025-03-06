189ba3ba3959c5f9ac78dd6a6b9eb31917b9b4bb
Olivia Boone <144468291+Olivia9444@users.noreply.github.com>
2025-03-05 20:59:21 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// console.log("Hello! Is tgus working?")
+
+var mainTitle = document.querySelector("h1");
+console.log(mainTitle.outerHTML);
+
+mainTitle.innerText = " Yay, JavaScript!";
+
+var body = document.querySelector("body");
+
+console.log(body);
+
+body.style.backgroundColor = "purple";
